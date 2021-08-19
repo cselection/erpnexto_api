fe4717ad762c7bd61afa6affbd312409a02eb1e2
cselection <mohamedyossif577@gmail.com>
2021-08-19 16:59:40 +0200

    zxc

---
+++ app/views.py
@@ -0,0 +1,34 @@
+from app import app
+from flask import jsonify, request
+from flask_cors import CORS, cross_origin
+import os
+
+cors = CORS(app, resource={
+    r"/*":{
+        "origins":"http://127.0.0.1:3000"
+    }
+})
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/signup", methods=['POST'])
+@cross_origin()
+def signup():
+    request_data = request.get_json()
+    site_name = None
+    business_mail = None
+    phone = None
+    password = None
+    if request_data:
+        if 'site_name' in request_data:
+            site_name = request_data['site_name']
+        if 'business_mail' in request_data:    
+            business_mail = request_data['business_mail']
+        if 'phone' in request_data:
+            phone = request_data['phone']
+        if 'password' in request_data:
+            password = request_data['password']
+        os.system('python script.py')
+        return jsonify(message="you have registered successfully")
+
+
+
---
+++ script.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import getopt
+import dns.resolver
+from subprocess import call
+import time
+
+def main(argv):
+    #get user data
+    site_name = argv[0]
+    business_mail = argv[1]
+    phone = argv[2]
+    password = argv[3]
+
+    #prepare some data
+    admin_password = 'Qwe-123'
+
+    #call dns server
+    dns_resolver = dns.resolver.Resolver()
+    dns_resolver.nameservers[0]
+
+    #call bench commands
+    call(["bench", "config", "dns-mulitenant", "on"])]
+    call(["bench", "new-site", site_name])]
+
+    #wait for the "Password:" prompt
+    time.sleep(2)
+
+    call([b])
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
